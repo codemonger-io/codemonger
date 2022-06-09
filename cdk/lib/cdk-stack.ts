10b356f49b8f8f523e35ad4bd49107749fa53bd4
Kikuo Emoto <kemoto@codemonger.io>
2022-06-09 18:57:13 +0900

    feat(cdk): provision S3 bucket for contents
    - A new CDK construct `ContentsBucket` defined in a new file
      `lib/contents-bucket.ts` provisions an S3 bucket to store contents for
      the codemonger website.
    - `CdkStack` outputs the name of the S3 bucket as "ContentsBucketName".
    
    issue codemonger-io/codemonger#2

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,6 +1,7 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
+import { ContentsBucket } from './contents-bucket';
 import { DeploymentStage } from './deployment-stage';
 
 type Props = StackProps & Readonly<{
@@ -12,5 +13,15 @@ export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props: Props) {
     super(scope, id, props);
 
+    const { deploymentStage } = props;
+
+    const contentsBucket = new ContentsBucket(this, 'ContentsBucket', {
+      deploymentStage,
+    });
+
+    new CfnOutput(this, 'ContentsBucketName', {
+      description: 'Name of the S3 bucket for the contents of codemonger website',
+      value: contentsBucket.bucket.bucketName,
+    });
   }
 }
