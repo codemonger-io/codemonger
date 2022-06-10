7faef75cb2d0453ba907eba50d9687409e9b13e4
Kikuo Emoto <kemoto@codemonger.io>
2022-06-10 10:04:17 +0900

    feat(cdk): provision contents distribution
    - A new CDK construct `ContentsDistribution` defined in a new file
      `lib/contents-distribution.ts` provisions a CloudFront distribution
      for contents of the codemonger website. It does not assign the
      official domain name (`codemonger.io`) yet.
    - `CdkStack` outputs the domain name of the CloudFront distribution as
      "ContentsDistributionDomainName".
    
    issue codemonger-io/codemonger#2

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -2,6 +2,7 @@ import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 import { ContentsBucket } from './contents-bucket';
+import { ContentsDistribution } from './contents-distribution';
 import { DeploymentStage } from './deployment-stage';
 
 type Props = StackProps & Readonly<{
@@ -18,10 +19,22 @@ export class CdkStack extends Stack {
     const contentsBucket = new ContentsBucket(this, 'ContentsBucket', {
       deploymentStage,
     });
+    const contentsDistribution = new ContentsDistribution(
+      this,
+      'ContentsDistribution',
+      {
+        contentsBucket,
+        deploymentStage,
+      },
+    );
 
     new CfnOutput(this, 'ContentsBucketName', {
-      description: 'Name of the S3 bucket for the contents of codemonger website',
+      description: 'Name of the S3 bucket for contents of the codemonger website',
       value: contentsBucket.bucket.bucketName,
+    });
+    new CfnOutput(this, 'ContentsDistributionDomainName', {
+      description: 'Domain name of the CloudFront distribution for contents of the codemonger website',
+      value: contentsDistribution.distribution.distributionDomainName,
     });
   }
 }
---
+++ cdk/lib/contents-distribution.ts
@@ -0,0 +1,66 @@
+import {
+  Duration,
+  aws_cloudfront as cloudfront,
+  aws_cloudfront_origins as origins,
+} from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+import { ContentsBucket } from './contents-bucket';
+import { DeploymentStage } from './deployment-stage';
+
+type Props = Readonly<{
+  // S3 bucket for contents of the codemonger website.
+  contentsBucket: ContentsBucket;
+  // Deployment stage.
+  deploymentStage: DeploymentStage;
+}>
+
+// TTL of the cache of each deployment stage.
+const CACHE_TTL_OF_STAGE = {
+  'development': Duration.seconds(1),
+  'production': Duration.minutes(10), // TODO: increase in the future
+} as const;
+
+/**
+ * CDK construct that provisions a CloudFront distribution for contents of the
+ * codemonger website.
+ *
+ * Traffic to `codemonger.io` will be served to the CloudFront distribution of
+ * the production stage.
+ */
+export class ContentsDistribution extends Construct {
+  /** CloudFront distribution for contents of the codemonger website. */
+  readonly distribution: cloudfront.IDistribution;
+
+  constructor(scope: Construct, id: string, props: Props) {
+    super(scope, id);
+
+    const { contentsBucket, deploymentStage } = props;
+
+    const cachePolicy = new cloudfront.CachePolicy(this, 'CachePolicy', {
+      comment: `codemonger contents cache policy (${deploymentStage})`,
+      defaultTtl: CACHE_TTL_OF_STAGE[deploymentStage],
+      minTtl: Duration.seconds(1),
+      enableAcceptEncodingBrotli: true,
+      enableAcceptEncodingGzip: true,
+    });
+
+    this.distribution = new cloudfront.Distribution(
+      this,
+      'ContentsDistribution',
+      {
+        comment: `codemonger distribution (${deploymentStage})`,
+        defaultBehavior: {
+          origin: new origins.S3Origin(contentsBucket.bucket),
+          cachePolicy,
+          // only static contents are served so far
+          cachedMethods: cloudfront.CachedMethods.CACHE_GET_HEAD,
+          viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        },
+        // do not add a slash (/) before the root object name.
+        defaultRootObject: 'index.html',
+        enableLogging: true,
+      },
+    );
+  }
+}
