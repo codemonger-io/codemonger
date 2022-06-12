8d7690809ba47bfa9c487cfbee05bb04527edc45
Kikuo Emoto <kemoto@codemonger.io>
2022-06-12 14:07:27 +0900

    feat(cdk): assign domain name
    - `ContentsDistribution` assigns the domain name `codemonger.io` to the
      CloudFront distribution for production. But as `codemonger.io` has
      already been used by the current CloudFront distribution, we have to
      transfer the domain name from the current one to the new one. We first
      have to create a CloudFront distribution without an alternate domain
      name then manually assign the SSL/TLS certificate to it before
      transferring the domain name. So a CDK context variable
      "codemonger:no-domain-name" that prevents domain name assignment to
      the CloudFront distribution is introduced.
    
    issue codemonger-io/codemonger#2

--- cdk/lib/contents-distribution.ts
+++ cdk/lib/contents-distribution.ts
@@ -2,11 +2,16 @@ import * as path from 'path';
 
 import {
   Duration,
+  aws_certificatemanager as acm,
   aws_cloudfront as cloudfront,
   aws_cloudfront_origins as origins,
 } from 'aws-cdk-lib';
 import { Construct, Node } from 'constructs';
 
+import {
+  CODEMONGER_DOMAIN_NAME,
+  CODEMONGER_CERTIFICATE_ARN,
+} from './certificate-config';
 import { ContentsBucket } from './contents-bucket';
 import { DeploymentStage } from './deployment-stage';
 
@@ -23,12 +28,19 @@ const CACHE_TTL_OF_STAGE = {
   'production': Duration.minutes(10), // TODO: increase in the future
 } as const;
 
+// name of the CDK context variable that specifies if the domain name
+// assignment to the CloudFront distribution is prevented.
+const NO_DOMAIN_NAME_CDK_CONTEXT = 'codemonger:no-domain-name';
+
 /**
  * CDK construct that provisions a CloudFront distribution for contents of the
  * codemonger website.
  *
  * Traffic to `codemonger.io` will be served to the CloudFront distribution of
  * the production stage.
+ * To prevent `codemonger.io` from being associated with the CloudFront
+ * distribution, specify `"true"` to the CDK context variable
+ * `codemonger:no-domain-name`.
  */
 export class ContentsDistribution extends Construct {
   /** CloudFront distribution for contents of the codemonger website. */
@@ -46,6 +58,18 @@ export class ContentsDistribution extends Construct {
       enableAcceptEncodingBrotli: true,
       enableAcceptEncodingGzip: true,
     });
+
+    // domain name and certificate are configured only for the production stage
+    const noDomain = isNoDomainName(Node.of(this));
+    const certificateParams =
+      deploymentStage === 'production' && !noDomain ? {
+        domainNames: [CODEMONGER_DOMAIN_NAME],
+        certificate: acm.Certificate.fromCertificateArn(
+          this,
+          'CodemongerDomainCertificate',
+          CODEMONGER_CERTIFICATE_ARN,
+        ),
+      } : {};
 
     const expandIndexFn = new cloudfront.Function(this, 'ExpandIndexFunction', {
       // provides a fixed function name because there is a bug in CDK that may
@@ -82,7 +106,26 @@ export class ContentsDistribution extends Construct {
         // do not add a slash (/) before the root object name.
         defaultRootObject: 'index.html',
         enableLogging: true,
+        ...certificateParams,
       },
     );
   }
+}
+
+// Returns if the domain name assignment to the CloudFront distribution is
+// prevented.
+//
+// The configuration is read from the CDK context.
+//
+// Returns `false` if the CDK context value is other than `true` (boolean) or
+// `"true"` (case-insensitive string).
+function isNoDomainName(node: Node): boolean {
+  const result = node.tryGetContext(NO_DOMAIN_NAME_CDK_CONTEXT);
+  if (typeof result === 'boolean') {
+    return result;
+  }
+  if (typeof result !== 'string') {
+    return false;
+  }
+  return result.toLowerCase() === 'true';
 }
