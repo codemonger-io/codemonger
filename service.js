46b1a9c410762591b6d7f72a0aa2e323c316bed0
Kikuo Emoto <kemoto@codemonger.io>
2022-06-19 13:51:25 +0900

    chore: delete the top-level package
    - `package.json`, `package-lock.json`, and `service.js` are delete
      because they are no longer necessary.

--- service.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const path = require('path')
-
-const port = 3000
-const app = express()
-
-app.use('/', express.static(path.resolve(__dirname, './docs')))
-
-app.listen(port, () => console.log(`listening at http://localhost:${port}`))
